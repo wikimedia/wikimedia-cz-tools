ec3fe6a0ecc10627614ea64c4f83030918effbc7
Martin Urbanec <martin@urbanec.cz>
2017-12-06 15:25:01 +0100

    Store tokens locally

--- quickstart.py
+++ quickstart.py
@@ -31,8 +31,7 @@ def get_credentials():
 	Returns:
 		Credentials, the obtained credential.
 	"""
-	home_dir = os.path.expanduser('~')
-	credential_dir = os.path.join(home_dir, '.credentials')
+	credential_dir = 'credentials'
 	if not os.path.exists(credential_dir):
 		os.makedirs(credential_dir)
 	credential_path = os.path.join(credential_dir,
