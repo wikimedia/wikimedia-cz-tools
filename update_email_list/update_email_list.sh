1a31b01f5f5ff9ed3f91dba142872f3b5186b0e9
Martin Urbanec <martin@urbanec.cz>
2018-05-14 13:16:21 +0000

    Fix path

--- update_email_list/update_email_list.sh
+++ update_email_list/update_email_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/urbanecm/Documents/wikimedia/wmcz/gsuite-tools/update_email_list
+cd /home/urbanecm/Documents/git/wmcz/gsuite-tools/update_email_list
 source ../venv/bin/activate
 python update_email_list.py
