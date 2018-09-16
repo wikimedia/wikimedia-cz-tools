5aa03dcf24c9a9b56e5d87b58e0025effa20d782
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-16 19:25:44 +0200

    Use absolute path to sql

--- purge_members_data/purge_members_data.sh
+++ purge_members_data/purge_members_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo 'DELETE FROM revision WHERE rev_page=100 AND rev_timestamp < (CURRENT_TIMESTAMP - INTERVAL 1 YEAR);' | sql inner_wiki && php /var/www/wiki.wikimedia.cz/mw/maintenance/purgeOldText.php --wiki=inner_wiki --purge
+echo 'DELETE FROM revision WHERE rev_page=100 AND rev_timestamp < (CURRENT_TIMESTAMP - INTERVAL 1 YEAR);' | /home/urbanecm/bin/sql inner_wiki && php /var/www/wiki.wikimedia.cz/mw/maintenance/purgeOldText.php --wiki=inner_wiki --purge
