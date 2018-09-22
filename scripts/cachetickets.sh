e75f7a94ba4f422dbb1a667945efc18269e15c34
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-23 00:50:11 +0200

    When caching tickets in Tracker2, use Tracker2's venv
    Change-Id: Ic325102cfc7a984b6db12115790af7370e203450

--- scripts/cachetickets.sh
+++ scripts/cachetickets.sh
@@ -2,4 +2,6 @@
 
 source /var/www/tracker.wikimedia.cz/deploy/pyenv/bin/activate
 python /var/www/tracker.wikimedia.cz/trackersite/manage.py cachetickets
+deactivate
+source /var/www/tracker2.wikimedia.cz/deploy/pyenv/bin/activate
 python /var/www/tracker2.wikimedia.cz/trackersite/manage.py cachetickets
