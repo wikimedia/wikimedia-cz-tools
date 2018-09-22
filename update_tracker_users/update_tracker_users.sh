e3ec944b609702d1f43b03bd0ff0803edda26c49
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-22 23:50:16 +0200

    Remove unneeded tool
    Change-Id: I053a83f2a52c900c5cc2d71cf52e16c67f9d25bd

--- update_tracker_users/update_tracker_users.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh masaryk /home/urbanecm/bin/sqldump tracker_main | mysql tracker_prod # Sync production database
-cd /home/urbanecm/Documents/git/wmcz/gsuite-tools/update_tracker_users
-source ../venv/bin/activate
-python update_tracker_users.py
