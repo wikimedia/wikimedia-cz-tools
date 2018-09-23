81256d401226c8888665911bc83da491a775fe4f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-23 19:11:17 +0200

    Fix paths
    ~urbanecm/bin/sql got moved to /usr/local/bin, adapt the scripts
    
    Change-Id: If50898140ca2be42ca7a542639c77dd8d8765efb

--- scripts/sendNotifications.sh
+++ scripts/sendNotifications.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /var/www/tracker.wikimedia.cz/deploy/pyenv/bin/activate
-echo "select * from tracker_notification" | /home/urbanecm/bin/sql tracker_main > /home/urbanecm/tmp/last_tracker_notification_tracker_main.txt
-echo "select * from tracker_notification" | /home/urbanecm/bin/sql tracker_test > /home/urbanecm/tmp/last_tracker_notification_tracker_test.txt
+echo "select * from tracker_notification" | sql tracker_main > /home/urbanecm/tmp/last_tracker_notification_tracker_main.txt
+echo "select * from tracker_notification" | sql tracker_test > /home/urbanecm/tmp/last_tracker_notification_tracker_test.txt
 python /var/www/tracker.wikimedia.cz/trackersite/manage.py sendnotifications
 python /var/www/tracker2.wikimedia.cz/trackersite/manage.py sendnotifications
