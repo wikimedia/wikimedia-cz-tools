c189a4cca41d6fd6a69ea66a56746ad54cb079da
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-10-06 19:12:39 +0200

    Switch VENVs before distributing notifications for Tracker2
    Change-Id: I0c8d67f83fa95b5e5de6987fee4d9d3877bd0f12

--- scripts/sendNotifications.sh
+++ scripts/sendNotifications.sh
@@ -4,4 +4,6 @@ source /var/www/tracker.wikimedia.cz/deploy/pyenv/bin/activate
 echo "select * from tracker_notification" | sql tracker_main > /home/urbanecm/tmp/last_tracker_notification_tracker_main.txt
 echo "select * from tracker_notification" | sql tracker_test > /home/urbanecm/tmp/last_tracker_notification_tracker_test.txt
 python /var/www/tracker.wikimedia.cz/trackersite/manage.py sendnotifications
+deactivate
+source /var/www/tracker2.wikimedia.cz/deploy/pyenv/bin/activate
 python /var/www/tracker2.wikimedia.cz/trackersite/manage.py sendnotifications
