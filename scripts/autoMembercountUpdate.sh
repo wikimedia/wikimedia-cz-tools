390a6112fcfda7778380b4497e4e62f413106378
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-12-19 13:09:02 +0100

    Wiki location was moved
    Change-Id: I1ac88169d587d78aeffa06cac1803bc6215cd10c

--- scripts/autoMembercountUpdate.sh
+++ scripts/autoMembercountUpdate.sh
@@ -3,11 +3,11 @@
 pocetRadnychClenu=$(echo 'select count(*) from user where user_name not in (select ipb_address from ipblocks) and user_id not in (select ug_user from user_groups where ug_group in ("bot", "techaccount"));' | mysql wikiusers | sed 1d)
 
 # Get things from wiki
-pocetSymClenu=$(php /var/www/wiki.wikimedia.cz/mw/maintenance/getText.php --wiki=inner_wiki "Členové/Počet sympatizujících členů")
-pocetCestClenu=$(php /var/www/wiki.wikimedia.cz/mw/maintenance/getText.php --wiki=inner_wiki "Členové/Počet čestných členů")
+pocetSymClenu=$(php /var/www/wikis/mw/maintenance/getText.php --wiki=innerwiki "Členové/Počet sympatizujících členů")
+pocetCestClenu=$(php /var/www/wikis/mw/maintenance/getText.php --wiki=innerwiki "Členové/Počet čestných členů")
 
-echo $(($pocetSymClenu+$pocetCestClenu+$pocetRadnychClenu)) | php /var/www/wiki.wikimedia.cz/mw/maintenance/edit.php --wiki=inner_wiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Celkový_počet_členů
-echo $pocetRadnychClenu | php /var/www/wiki.wikimedia.cz/mw/maintenance/edit.php --wiki=inner_wiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Počet_řádných_členů
-echo Členové | php /var/www/wiki.wikimedia.cz/mw/maintenance/purgePage.php --wiki=inner_wiki -q
-echo $pocetRadnychClenu | php  /var/www/www.wikimedia.cz/mw/maintenance/edit.php --wiki=pub_wiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Lidé/Počet_řádných_členů
-echo Lidé | php /var/www/www.wikimedia.cz/mw/maintenance/purgePage.php --wiki=pub_wiki -q
+echo $(($pocetSymClenu+$pocetCestClenu+$pocetRadnychClenu)) | php /var/www/wikis/mw/maintenance/edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Celkový_počet_členů
+echo $pocetRadnychClenu | php /var/www/wikis/mw/maintenance/edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Počet_řádných_členů
+echo Členové | php /var/www/wikis/mw/maintenance/purgePage.php --wiki=innerwiki -q
+echo $pocetRadnychClenu | php  /var/www/wikis/mw/maintenance/edit.php --wiki=pubwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Lidé/Počet_řádných_členů
+echo Lidé | php /var/www/wikis/mw/maintenance/purgePage.php --wiki=pubwiki -q
