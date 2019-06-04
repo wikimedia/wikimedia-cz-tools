c2a54d928aa1ccbcfc96bec666747ae5b413a3a4
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-06-04 15:31:21 +0200

    Implement parametrized emails
    Change-Id: I8f6845385d5a0cf9727bc4dd79246d5af22be64c

--- sendmails/sendmails.py
+++ sendmails/sendmails.py
@@ -8,13 +8,17 @@ from email.mime.multipart import MIMEMultipart
 
 @click.command()
 @click.option('--subject', default='(bez předmětu)')
-@click.option('--addresses', required=True)
+@click.option('--data', required=True)
 @click.option('--from', 'from_mail', default='martin.urbanec@wikimedia.cz')
 @click.option('--from-name', 'from_name', default='Martin Urbanec')
 @click.option('--mail', 'mail_file', required=True)
 @click.option('--smtp-server', default='smtp-relay.gmail.com')
-def mails(subject, addresses, mail_file, from_mail, from_name, smtp_server):
-	mails = open(addresses).readlines()
+def mails(subject, data, mail_file, from_mail, from_name, smtp_server):
+	data = open(data).readlines()
+	if len(data[0].split('\t')) > 1:
+		header = data.pop(0).replace('\n', '').split('\t')
+	else:
+		header = None
 
 	orig_mailtext = open(mail_file).read()
 	mailtext = []
@@ -25,14 +29,26 @@ def mails(subject, addresses, mail_file, from_mail, from_name, smtp_server):
 	s = smtplib.SMTP(smtp_server)
 	s.ehlo()
 	s.starttls()
-	for mail in mails:
-		mail = mail.replace('\n', '')
+	data2 = {}
+	for line in data:
+		row = line.replace('\n', '').split('\t')
+		data2[row[header.index('mail')]] = {}
+		for var in header:
+			if var == 'mail':
+				continue
+			data2[row[header.index('mail')]][var] = row[header.index(var)]
+	data = data2
+
+	for mail in data:
 		msg = MIMEMultipart('alternative')
 		msg['Subject'] = subject
 		msg['From'] = '%s <%s>' % (from_name, from_mail)
 		msg['To'] = mail
 
-		html = MIMEText(mailtext, 'html')
+		text = mailtext
+		for var in data[mail]:
+			text = text.replace('@@%s@@' % var, data[mail][var])
+		html = MIMEText(text, 'html')
 		msg.attach(html)
 		s.sendmail(from_mail, mail, msg.as_string())
 	s.quit()
