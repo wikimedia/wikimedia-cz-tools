c1c9e618a19feb886193f0534149a29b436290a9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-01-20 18:50:20 +0000

    Remove debug code
    Change-Id: I0a48c0aa9270575f598cb49071a92d14e69f5c9a

--- scripts/sendNotifications.sh
+++ scripts/sendNotifications.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 source /var/www/tracker.wikimedia.cz/deploy/pyenv/bin/activate
-echo "select * from tracker_notification" | sql tracker_main > /home/urbanecm/tmp/last_tracker_notification_tracker_main.txt
-echo "select * from tracker_notification" | sql tracker_test > /home/urbanecm/tmp/last_tracker_notification_tracker_test.txt
 python /var/www/tracker.wikimedia.cz/trackersite/manage.py sendnotifications
 deactivate
 source /var/www/tracker2.wikimedia.cz/deploy/pyenv/bin/activate
