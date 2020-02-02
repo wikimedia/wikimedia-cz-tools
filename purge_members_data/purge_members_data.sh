9ee7b31d05a206e1929d7dabdf78d3c5a1347aac
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-02-02 23:38:25 +0100

    Use mysql instead of sql
    Change-Id: Idf05eee9b39fa0c54c03c6dbd726547f6ab303c7

--- purge_members_data/purge_members_data.sh
+++ purge_members_data/purge_members_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo 'DELETE FROM revision WHERE rev_page=100 AND rev_timestamp < (CURRENT_TIMESTAMP - INTERVAL 1 YEAR);' | sql innerwiki && php /var/www/wikis/mw/maintenance/purgeOldText.php --wiki=innerwiki --purge
+echo 'DELETE FROM revision WHERE rev_page=100 AND rev_timestamp < (CURRENT_TIMESTAMP - INTERVAL 1 YEAR);' | mysql innerwiki && php /var/www/wikis/mw/maintenance/purgeOldText.php --wiki=innerwiki --purge
