f65512d2e65e5235d5edd4f43b18247863a5c43c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-30 15:07:56 +0000

    wmcz_web_posts_p: Dump individual tables as well
    Change-Id: I5e46a0616fd339c09500b50d52f95eed77c49c16

--- update_reports/publish_reports.sh
+++ update_reports/publish_reports.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir -p /var/www/files.wikimedia.cz/datasets
+mkdir -p /var/www/files.wikimedia.cz/datasets/web-posts
 
 mysqldump wmcz_web_posts_p | gzip > /var/www/files.wikimedia.cz/datasets/web-posts/wmcz_web_posts_p.sql.gz
+
+for table in blogposts news_category news_tags news_web; do
+	mysql wmcz_web_posts_p -e "SELECT * FROM $table" > /var/www/files.wikimedia.cz/datasets/web-posts/$table.tsv
+done
