cd091ae6141ef2052439325d1f58a6f6508a9422
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-06-01 09:27:31 +0000

    autoMembercountUpdate: Update to meet new table structure
    Change-Id: Ia7c848c60e1c007de1c6974fe5b4a0aacda5e80e

--- scripts/autoMembercountUpdate.sh
+++ scripts/autoMembercountUpdate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-pocetRadnychClenu=$(echo 'select count(*) from user where user_name not in (select ipb_address from ipblocks) and user_id not in (select ug_user from user_groups where ug_group in ("bot", "techaccount", "nonmember"));' | mysql wikiusers | sed 1d)
+set -e
+
+pocetRadnychClenu=$(echo 'select count(*) from user where user_id not in (select bt_user from block join block_target on bl_target=bt_id where bt_user is not null) and user_id not in (select ug_user from user_groups where ug_group in ("bot", "techaccount", "nonmember"))' | mysql wikiusers | sed 1d)
 
 # Get things from wiki
-pocetSymClenu=$(php /var/www/wikis/mw/maintenance/getText.php --wiki=innerwiki "Členové/Počet sympatizujících členů")
-pocetCestClenu=$(php /var/www/wikis/mw/maintenance/getText.php --wiki=innerwiki "Členové/Počet čestných členů")
+pocetSymClenu=$(php /var/www/wikis/mw/maintenance/run.php getText.php --wiki=innerwiki "Členové/Počet sympatizujících členů")
+pocetCestClenu=$(php /var/www/wikis/mw/maintenance/run.php getText.php --wiki=innerwiki "Členové/Počet čestných členů")
 
-echo $(($pocetSymClenu+$pocetCestClenu+$pocetRadnychClenu)) | php /var/www/wikis/mw/maintenance/edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Celkový_počet_členů
-echo $pocetRadnychClenu | php /var/www/wikis/mw/maintenance/edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --user=UrbanecmBot Členové/Počet_řádných_členů
-echo Členové | php /var/www/wikis/mw/maintenance/purgePage.php --wiki=innerwiki -q
+echo $(($pocetSymClenu+$pocetCestClenu+$pocetRadnychClenu)) | php /var/www/wikis/mw/maintenance/run.php edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --quiet --user=UrbanecmBot Členové/Celkový_počet_členů
+echo $pocetRadnychClenu | php /var/www/wikis/mw/maintenance/run.php edit.php --wiki=innerwiki --summary="Robot: Aktualizace počtu členů" --bot --minor --quiet --user=UrbanecmBot Členové/Počet_řádných_členů
+echo Členové | php /var/www/wikis/mw/maintenance/run.php purgePage.php --wiki=innerwiki -q
